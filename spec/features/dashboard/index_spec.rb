36cdc0b76eeaf5922cb86a21df13823fffd37a55
JoannaCoPo <jococopo@gmail.com>
2021-06-12 16:13:36 -0600

    Create test for link to bulk discounts

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -116,4 +116,13 @@ RSpec.describe 'merchant dashboard' do
   it "shows the date that the invoice was created in this format: Monday, July 18, 2019" do
     expect(page).to have_content(@invoice_1.created_at.strftime("%A, %B %-d, %Y"))
   end
+
+  # ----------------------------------------------------------------------------------------------------------
+  it 'has a link to all discounts that redircts to bulk discount index' do
+    within ".nav-container" do
+      expect(page).to have_link('All Bulk Discounts')
+      click_link('All Bulk Discounts')
+      expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
+    end
+  end
 end
