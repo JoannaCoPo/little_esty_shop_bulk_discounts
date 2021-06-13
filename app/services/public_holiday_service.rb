8855ddbfba7cad7e68ee22e39533b11871b3040b
JoannaCoPo <jococopo@gmail.com>
2021-06-12 21:48:23 -0600

    Create public holiday service file for api

---
+++ app/services/public_holiday_service.rb
@@ -0,0 +1,15 @@
+require 'faraday'
+require 'json'
+
+class PublicHolidayService
+  attr_reader :upcoming_3_holidays
+
+  def initialize
+    @upcoming_3_holidays = next_three
+  end
+
+  def next_three
+    response = Faraday.get("https://date.nager.at/api/v2/NextPublicHolidays/us")
+    JSON.parse(response.body, symbolize_names: true)[0..2]
+  end
+end
