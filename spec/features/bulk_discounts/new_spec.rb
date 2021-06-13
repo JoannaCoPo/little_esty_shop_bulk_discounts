602b36b1230d14abf4f6b39d2e934544d9a42fd4
JoannaCoPo <jococopo@gmail.com>
2021-06-13 16:07:23 -0600

    Remove save_and_open_page"

--- spec/features/bulk_discounts/new_spec.rb
+++ spec/features/bulk_discounts/new_spec.rb
@@ -8,14 +8,12 @@ RSpec.describe 'merchant bulk discounts new' do
   end
 
   it 'has a form to create a new bulk discount' do
-
     fill_in 'Discount', with: 50
     fill_in 'Min quantity', with: 25
     click_button 'submit'
 
     expect(current_path).to eq("/merchant/#{@merchant_1.id}/bulk_discounts")
     expect(page).to have_content(50.0)
-    save_and_open_page
     expect(page).to have_content(25)
   end
 end
