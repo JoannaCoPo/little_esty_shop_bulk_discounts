f3da6ebf119cc7ee1b673a490f4e6f45ecd303b7
JoannaCoPo <jococopo@gmail.com>
2021-06-13 18:11:22 -0600

    Create tests for bulk discount show page

---
+++ spec/features/bulk_discounts/show_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'merchant bulk discounts show' do
+  before :each do
+    @merchant_1 = Merchant.create!(name: "Fancy Petz")
+    @bulk_discount_1 = @merchant_1.bulk_discounts.create!(discount: 20, min_quantity: 15)
+    @bulk_discount_2 = @merchant_1.bulk_discounts.create!(discount: 10, min_quantity: 10)
+  end
+
+  it 'displays the bulk discounts quantity threshold and percentage discount' do
+    visit merchant_bulk_discount_path(@merchant_1, @bulk_discount_1)
+
+    expect(page).to have_content(@bulk_discount_1.discount)
+    expect(page).to have_content(@bulk_discount_1.min_quantity)
+
+    visit merchant_bulk_discount_path(@merchant_1, @bulk_discount_2)
+
+    expect(page).to have_content(@bulk_discount_2.discount)
+    expect(page).to have_content(@bulk_discount_2.min_quantity)
+  end
+end
